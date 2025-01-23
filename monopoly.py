aaa6a39d7b01751daf3c248ef83985a06fb707c5
Ayesha Akheel <akheelayesha2000@gmail.com>
2025-01-23 14:08:57 +1100

    Implement player movement method with board wrapping

--- monopoly.py
+++ monopoly.py
@@ -9,4 +9,7 @@ class Player:
         self.score = 0
         self.money = 16 # starting money
         self.properties = [] #properties owned by player
-        
+    
+    def move(self, steps, board_size):
+        """Move the layer around the board, wrapping around if necessary."""
+        self.position = (self.position + steps) % board_size
