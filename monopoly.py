d797bdc57ea0d60934728e6d22fdacf97d3242ee
Ayesha Akheel <akheelayesha2000@gmail.com>
2025-01-23 15:07:45 +1100

    Add buy_property method to Player class

--- monopoly.py
+++ monopoly.py
@@ -1,8 +1,8 @@
 import json
 import random
 
-# Player class
 
+# Player class
 class Player:
     def __init__(self, name):
         self.name = name
@@ -13,3 +13,12 @@ class Player:
     def move(self, steps, board_size):
         """Move the layer around the board, wrapping around if necessary."""
         self.position = (self.position + steps) % board_size
+
+    def buy_property(self, property):
+        """Buys the property if the player has enough money."""
+        if self.money >= property['price']:
+            self.money -= property['price']
+            self.properties.append(property)
+            print(f"{self.name} buys {property['name']} for ${property['price']}")
+        else:
+            print(f"{self.name} cannot afford {property['name']}!")
